2e76926235455266fe790bbca6f92a902f5a7ba6
Sharvani Haran <sharvaniharan@yahoo.com>
2015-05-14 04:10:41 -0700

    Initial commit

---
+++ app/src/main/java/com/example/sharvani/imageauthapp/PointCollecterListener.java
@@ -0,0 +1,9 @@
+package com.example.sharvani.imageauthapp;
+
+import android.graphics.Point;
+
+import java.util.List;
+
+public interface PointCollecterListener {
+	public void pointsCollected(List<Point> points);
+}
---
+++ app/src/main/java/com/example/sharvani/imageauthapp/PointCollector.java
@@ -0,0 +1,42 @@
+package com.example.sharvani.imageauthapp;
+
+import android.graphics.Point;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PointCollector implements OnTouchListener {
+
+	public static final int NUM_POINTS = 4;
+	private PointCollecterListener listener;
+	private List<Point> points = new ArrayList<Point>();
+
+	public boolean onTouch(View v, MotionEvent event) {
+
+		int x = Math.round(event.getX());
+		int y = Math.round(event.getY());
+
+		points.add(new Point(x, y));
+		
+		if(points.size() == NUM_POINTS) {
+			if(listener != null) {
+				listener.pointsCollected(points);
+			}
+		}
+
+		return false;
+	}
+
+	public void setListener(PointCollecterListener listener) {
+		this.listener = listener;
+	}
+	
+	public void clear() {
+		points.clear();
+	}
+}
+
+
